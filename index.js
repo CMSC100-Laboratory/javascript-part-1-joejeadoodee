52d1343cc4da6584ee5972f222b2a9d8b6d1a818
joejeadoodee <jalonzo@up.edu.ph>
2025-02-19 20:48:45 +0800

    slightly modified reversePassword() & wrote code for storePassword()

--- index.js
+++ index.js
@@ -34,9 +34,8 @@ function isValid(str){
 }
 
 function reversePassword(str){
-    console.log(str);
-    if(isValid){
-        var newStr = "", i;
+    if(isValid(str)){
+        let newStr = "", i;
         for(i = str.length - 1; i >= 0; i--) { 
             newStr += str[i];
         }
@@ -47,16 +46,26 @@ function reversePassword(str){
 }
 
 function storePassword(name, str1, str2){
+    let result = {
+        name: name,
+        newpassword: str1,
+    }
 
+    if(validatePassword(str1, str2)){
+        result.newpassword = reversePassword(str1);
+    }
+
+    return result;
 }
 
-// console.log(storePassowrd("John", "Pass1234", "Pass1234"));
-// console.log(storePassowrd("John", "Pass123", "Pass12345"));
+console.log(storePassword("John", "Pass1234", "Pass1234"));
+console.log(storePassword("John", "Pass123", "Pass12345"));
 
 // for test cases only
-console.log(isValid("hEl0ssg2"));
-console.log(isValid("hEl0"));
-console.log(reversePassword(("hEl0")));
-console.log(reversePassword("hEl0ssg2"));
-console.log(validatePassword("Heww1122", "Heww1122"));
-console.log(validatePassword("Heww1122a", "Heww1122"));
+//console.log(isValid("hEl0ssg2"));
+//console.log(isValid("hEl0"));
+// console.log(validatePassword("Heww1122", "Heww1122"));
+// console.log(validatePassword("Heww1122a", "Heww1122"));
+// console.log(reversePassword(("hEl0")));
+// console.log(reversePassword("hEl0ssg2"));
+// console.log(reversePassword("Hello1234")); 
