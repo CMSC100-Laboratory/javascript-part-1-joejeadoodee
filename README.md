4b24519228515e810e10b96084e30c5d86459554
joejeadoodee <jalonzo@up.edu.ph>
2025-02-19 20:31:47 +0800

    home commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/5v8RsmJk)
+
+# Week 03: Exercise 3 Javascript Basics part 1
+
+## Student Information
+
+Name: Joejean V. Alonzo
+
+Student Number: 2022-69593
+
+Section: C-2L
+
+## Application Description
+Terminal-based application to validate if password is valid, if 2 password matched.
+
+## References Used
+https://stackoverflow.com/questions/26322867/how-to-validate-password-using-following-conditions
+
+https://u-next.com/blogs/java/what-is-password-validation-in-javascript-beginners-guide/
---
+++ index.js
@@ -0,0 +1,62 @@
+function validatePassword(str1, str2) { 
+    if(isValid(str1) && isValid(str2)){ // if valid password
+        if(str1.length == str2.length){ // check size, check content 
+            str1 === str2;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    return false;
+}
+
+function isValid(str){
+    if(str.length >= 8){
+        // check for uppercase char
+        for (i=0; i < str.length; i++){
+            str.search(/[A-Z]/);
+            uppercase = true;
+        }
+        // check for lowercase char
+        for(i=0; i < str.length; i++){
+            str.search(/[a-z]/);
+            lowercase = true;
+        }
+        //check for num char
+        for(i=0; i < str.length; i++){
+            str.search(/[0-9]/);
+            number = true;
+        }
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function reversePassword(str){
+    console.log(str);
+    if(isValid){
+        var newStr = "", i;
+        for(i = str.length - 1; i >= 0; i--) { 
+            newStr += str[i];
+        }
+        return newStr;
+    }
+    console.log("cannot be reverse, invalid password");
+    return str;
+}
+
+function storePassword(name, str1, str2){
+
+}
+
+// console.log(storePassowrd("John", "Pass1234", "Pass1234"));
+// console.log(storePassowrd("John", "Pass123", "Pass12345"));
+
+// for test cases only
+console.log(isValid("hEl0ssg2"));
+console.log(isValid("hEl0"));
+console.log(reversePassword(("hEl0")));
+console.log(reversePassword("hEl0ssg2"));
+console.log(validatePassword("Heww1122", "Heww1122"));
+console.log(validatePassword("Heww1122a", "Heww1122"));
